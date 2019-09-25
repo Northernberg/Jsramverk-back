63cf0145a95791f47d36b50b1465ed4ca85da93b
Gustav Nordberg <Gustav.123@live.se>
2019-09-25 13:15:11 +0200

    remove validate for password

--- routes/login.js
+++ routes/login.js
@@ -8,42 +8,38 @@ const secret = process.env.JWT_SECRET;
 const { check, validationResult } = require('express-validator');
 const bcrypt = require('bcryptjs');
 
-router.post(
-  '/',
-  [check('email').isEmail(), check('password').isLength({ min: 5 })],
-  (req, res) => {
-    const errors = validationResult(req);
-    if (!errors.isEmpty()) {
-      return res.status(422).json({ errors: errors.array() });
-    } else {
-      db.get(
-        'SELECT * FROM USERS WHERE email = ?',
-        req.body.email,
-        (err, row) => {
-          if (err) {
-            return res.status(422).send('Wrong email or password');
-          } else {
-            bcrypt.compare(req.body.password, row.password, function(
-              err,
-              correct
-            ) {
-              if (err || !correct) {
-                return res.status(422).send('Wrong Password');
-              } else if (correct) {
-                const payload = { email: 'user@example.com' };
-                const token = jwt.sign(payload, secret, { expiresIn: '1h' });
+router.post('/', [check('email').isEmail()], (req, res) => {
+  const errors = validationResult(req);
+  if (!errors.isEmpty()) {
+    return res.status(422).json({ errors: errors.array() });
+  } else {
+    db.get(
+      'SELECT * FROM USERS WHERE email = ?',
+      req.body.email,
+      (err, row) => {
+        if (err) {
+          return res.status(422).send('Wrong email or password');
+        } else {
+          bcrypt.compare(req.body.password, row.password, function(
+            err,
+            correct
+          ) {
+            if (err || !correct) {
+              return res.status(422).send('Wrong Password');
+            } else if (correct) {
+              const payload = { email: 'user@example.com' };
+              const token = jwt.sign(payload, secret, { expiresIn: '1h' });
 
-                res.status(200).json({
-                  username: row.firstname,
-                  token: token,
-                });
-              }
-            });
-          }
+              res.status(200).json({
+                username: row.firstname,
+                token: token,
+              });
+            }
+          });
         }
-      );
-    }
+      }
+    );
   }
-);
+});
 
 module.exports = router;
